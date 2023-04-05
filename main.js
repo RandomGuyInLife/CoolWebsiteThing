eba455ff5419dd015b265f232651a1306b26edb1
RandomGuyInLife <129981046+RandomGuyInLife@users.noreply.github.com>
2023-04-05 18:23:59 +0000

    5

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
 var body = document.getElementById("body");
-body.innerHTML += "Hi. This website is a work in proggrest and I would appreciate if you could wubmit ideas <a he>here
+body.innerHTML += "";
