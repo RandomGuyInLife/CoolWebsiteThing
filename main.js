b04d20260b40a23ed536af40c1ca56cd060da4d4
RandomGuyInLife <129981046+RandomGuyInLife@users.noreply.github.com>
2023-04-06 16:52:23 +0000

    eeeeeeeeeeweeeeeeeee

--- main.js
+++
@@ -1,6 +0,0 @@
-function somethin(){
-var body = document.getElementsById("main");
-var e = "<div id=\"invis\">_" + body.innerHTML + "_</div>";
-body.innerHTML = e.split("_")[1];
-document.getElementById("temp").innerHTML = "";
-}
