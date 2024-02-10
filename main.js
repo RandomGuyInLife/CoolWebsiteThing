db2355e4c33a759ed59b20c447394913230e0aa3
RandomGuyInLife <129981046+RandomGuyInLife@users.noreply.github.com>
2024-02-10 18:02:01 +0000

    plz work man

--- main.js
+++ main.js
@@ -4,17 +4,7 @@ head.style.height = screen.height + "px";
 const img = head.appendChild(document.createElement("img"));
 img.src = "AAF7eCm4sbw_sWriQm_1707515503330.png";
 img.style.height = "200px";
-
-const test = function () {
-  for (i = 0; i < 100;) {
-  setTimeout(() => {
-      head.style.top = i + "px";
-      i++;
-      head.textContent = head.style.top + "q2e2qe21e12e12e12e21e12e12e12ee1212";
-    
-  }, 5000);}
-}
-setTimeout(test(), 10000)
+head.style.top = 100 + "px";
 
 
 window.onscroll = function() {scrollFunction()};
