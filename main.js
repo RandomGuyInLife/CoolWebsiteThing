85da43e06992800959e3c287d562b752dc5e3ca1
RandomGuyInLife <129981046+RandomGuyInLife@users.noreply.github.com>
2024-02-10 18:00:15 +0000

    plz fix this

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const img = head.appendChild(document.createElement("img"));
 img.src = "AAF7eCm4sbw_sWriQm_1707515503330.png";
 img.style.height = "200px";
 
-const test = async () => {
+const test = function () {
   for (i = 0; i < 100;) {
   setTimeout(() => {
       head.style.top = i + "px";
@@ -14,7 +14,7 @@ const test = async () => {
     
   }, 5000);}
 }
-test();
+setTimeout(test(), 10000)
 
 
 window.onscroll = function() {scrollFunction()};
