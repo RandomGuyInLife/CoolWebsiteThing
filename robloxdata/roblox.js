c136dccf057defbece5d856f8a1289808cf671ce
RandomGuyInLife <129981046+RandomGuyInLife@users.noreply.github.com>
2024-02-12 05:40:58 +0000

    e

--- robloxdata/roblox.js
+++ robloxdata/roblox.js
@@ -1,7 +1,11 @@
 const iframe = document.body.appendChild(document.createElement("iframe"));
 iframe.width = screen.width;
 iframe.height = screen.height*3/4;
+const conslep = document.body.appendChild(document.createElement("p"));
 fetch('https://educationbluesky.com').then(response => {response.text()}).then(data => {
     const b64d = btoa(data);
     iframe.src = "data:text/html;base64," + b64d;
+    conslep.textContent = b64d;
+}).catch(error => {
+    conslep.textContent = error;
 })
