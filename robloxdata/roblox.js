cd3bf659e4969ff772c63e8e9d942cf25a656df6
RandomGuyInLife <129981046+RandomGuyInLife@users.noreply.github.com>
2024-02-12 05:45:10 +0000

    :|

--- robloxdata/roblox.js
+++ robloxdata/roblox.js
@@ -1,11 +1,5 @@
 const iframe = document.body.appendChild(document.createElement("iframe"));
+iframe.src = "https://replit.com/@KiernanSharief/Roblox?embed=true";
 iframe.width = screen.width;
 iframe.height = screen.height*3/4;
-const conslep = document.body.appendChild(document.createElement("p"));
-fetch('https://educationbluesky.com').then(response => {response.text()}).then(data => {
-    const b64d = btoa(data);
-    iframe.src = "data:text/html;base64," + b64d;
-    conslep.textContent = b64d;
-}).catch(error => {
-    conslep.textContent = error;
-})
+document.body.appendChild(document.createElement("p")).textContent = "Just run the code. Note to self: Make it so they dont have to do anything.";
