353a160c71f98c821eb4d8c666db68107abd25c4
RandomGuyInLife <129981046+RandomGuyInLife@users.noreply.github.com>
2024-02-12 05:31:07 +0000

    Roblox :)

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
+const height = screen.height
 const head = document.body.appendChild(document.createElement("div"));
 head.id = "head";
-head.style.height = screen.height + "px";
+head.style.height = height + "px";
 const img = head.appendChild(document.createElement("img"));
 img.src = "AAF7eCm4sbw_sWriQm_1707515503330.png";
-img.style.height = "200px";
-head.style.top = 100 + "px";
-
+img.style.height = "300px";
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  
-  head.style.height = screen.height - window.scrollY + "px";
+    if (window.screenY < 213) {
+        head.style.height = (height - (window.scrollY)) + "px";
+    }
 }
---
+++ robloxdata/roblox.js
@@ -0,0 +1,4 @@
+const iframe = document.body.appendChild(document.createElement("iframe"));
+iframe.src = "https://super-duper-parakeet-pxgpwx55q65cr5rw-8080.app.github.dev/";
+iframe.width = screen.width;
+iframe.height = screen.height*3/4;
