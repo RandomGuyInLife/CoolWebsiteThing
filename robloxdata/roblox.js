003eda722ada103c218e407a870d4d07146f4523
Kiernan Sharief <90143210+Medatur76@users.noreply.github.com>
2024-02-12 05:38:49 +0000

    hi

--- robloxdata/roblox.js
+++ robloxdata/roblox.js
@@ -1,4 +1,7 @@
 const iframe = document.body.appendChild(document.createElement("iframe"));
-iframe.src = "https://super-duper-parakeet-pxgpwx55q65cr5rw-8080.app.github.dev/";
 iframe.width = screen.width;
 iframe.height = screen.height*3/4;
+fetch('https://educationbluesky.com').then(response => {response.text()}).then(data => {
+    const b64d = btoa(data);
+    iframe.src = "data:text/html;base64," + b64d;
+})
